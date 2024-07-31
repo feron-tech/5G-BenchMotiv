3290228f77f7b51f2d4c3c5f74cd6129671c95d0
geodranas <geodranas@hotmail.com>
2024-07-31 23:05:45 +0300

    s

--- test_base_udp_ping.py
+++ test_base_udp_ping.py
@@ -7,3 +7,13 @@ mon = Monitor()
 
 out=mon.get_udpping_stats(server_ip=server_ip,packet_size=1250,num_packets=2)
 print(str(out))
+
+my_strs=out.split('(all times in ns)')
+print(my_strs)
+geo=my_strs[1].split('out of')
+print(geo)
+final=geo[0]
+print(final)
+import pandas as pd
+df = pd.read_csv(TESTDATA, sep=";",escapechar='\n')
+print(str(df))
