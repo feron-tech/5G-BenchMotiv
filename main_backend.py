181e399b66e8436d6e604429f74336fabaa8ceac
geodranas <geodranas@hotmail.com>
2024-08-02 15:20:50 +0300

    test-midband-exp6

--- main_backend.py
+++ main_backend.py
@@ -182,10 +182,10 @@ class Backend:
 		try:
 			_server_ip = self.df_in_user['in_set_server_ip'].iloc[0]
 			_iperf_duration=10
-			_camp_name = 'day2_midband_exp5'
-			_ping_interval = 0.010
+			_camp_name = 'day2_midband_exp6'
+			_ping_interval = 0.020
 			_ping_packs = 2000
-			_packet_size=100
+			_packet_size=512
 			print('(Backend) DBG: Get baseline measurements for ip='+str(_server_ip)+'...')
 
 			base_dict={}
