944b0c9e913d2ba4b39e7397a543289969e5218d
Georgios Drainakis <giorgos.drainakis@iccs.gr>
2024-08-29 13:38:19 +0300

    s

--- client/client_opencv_internet/src.py
+++ client/client_opencv_internet/src.py
@@ -1,12 +1,13 @@
 import cv2
 
 cap = cv2.VideoCapture('http://pendelcam.kip.uni-heidelberg.de/mjpg/video.mjpg')
-cv2.namedWindow('live cam', cv2.WINDOW_NORMAL)
+#cv2.namedWindow('live cam', cv2.WINDOW_NORMAL)
 
 while(True):
     ret, frame = cap.read()
-    img_resize = cv2.resize(frame, (960, 540))
-    cv2.imshow('live cam', frame)
+    print(str(frame))
+    #img_resize = cv2.resize(frame, (960, 540))
+    #cv2.imshow('live cam', frame)
     if cv2.waitKey(50) & 0xFF == ord('q'):
         break
 
